d663187e69869902b48bd9533598bb0bcccdafd4
BetoSandoval <alberto.sa1391@gmail.com>
2020-08-16 12:13:47 -0500

    Modificaciones en el codigo del archivo main.js

--- assets/Js/main.js
+++ assets/Js/main.js
@@ -1,6 +1,14 @@
 $(document).ready(function(){
+    const imgItems = $('.slider li').length; // Número de slider
+    console.log(imgItems);
 
-    $('.slider li').hide();
-    $('.slider li:first').show();
+    // Agregando paginación
+    // for(i = 1; i <= imgItems; i++){
+
+    // }
+
+    $('.slider li').hide(); // Ocultando todos los slider
+    $('.slider li:first').show(); // Muestra el primer slide
+    $('.paginacion li:first').css({'color': '#000000'}); // Damos estilos al primer item de la pagina
 
 });
