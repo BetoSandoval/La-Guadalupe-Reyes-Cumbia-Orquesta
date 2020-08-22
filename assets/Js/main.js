baebae682dae3166a08bee726a2acfd596931a4a
BetoSandoval <alberto.sa1391@gmail.com>
2020-08-21 21:10:31 -0500

    cambios control de scroll

--- assets/Js/main.js
+++ assets/Js/main.js
@@ -13,5 +13,15 @@ $(document).ready(function () {
 
   top.on('click', function(){
     $('html body').animate({scrollTop: 0}, 500);
-  })
+  });
+
+  top.hide();
+  $(window).scroll(function(){
+
+    if($(this).scrollTop() > 300){
+      top.fadeIn();
+    }else{
+      top.fadeOut();
+    }
+  });
 });
