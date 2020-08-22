196e7c413b293791cb8a47fd12a43eee743bf14c
BetoSandoval <alberto.sa1391@gmail.com>
2020-08-21 19:54:37 -0500

    boton top

--- assets/Js/main.js
+++ assets/Js/main.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
   let boton = $(".hamburguer");
   let menu = $("header nav");
+  let top = $('.top ');
 
   boton.on('click', function () {
     menu.slideToggle(1000);
@@ -9,4 +10,8 @@ $(document).ready(function () {
   menu.on('click', function() {
       menu.fadeOut();
   });
+
+  top.on('click', function(){
+    $('html body').animate({scrollTop: 0}, 500);
+  })
 });
